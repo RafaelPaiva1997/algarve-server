c94b7aa02fe35fa873fd4e4f504a4b82405a6ef1
Rafael Paiva <rafael.neves1infor2@gmail.com>
2018-12-27 02:13:50 +0000

    finale

--- controllers/emails.js
+++ controllers/emails.js
@@ -1,18 +1,60 @@
-var db = require('../functionals/database');
-var req_res = require('../functionals/req_res');
+var db = require("../functionals/database");
+var req_res = require("../functionals/req_res");
+var email_lib = require("../functionals/email_lib");
 
 exports.add = (req, res) => {
-    db.insert(req.body.lib, req.body.data.item, req_res.res_err(res));
-}
+  db.insert(req.body.lib, req.body.data.item, req_res.res_err(res));
+};
+
+exports.addMany = (req, res) => {
+  db.insertMany(req.body.lib, req.body.data.items, req_res.res_err(res));
+};
 
 exports.update = (req, res) => {
-    db.update(req.body.lib, req.body.data.key, req.body.data.update, req_res.res_err_result(res));
-}
+  db.update(
+    req.body.lib,
+    req.body.data.key,
+    req.body.data.update,
+    req_res.res_err_result(res)
+  );
+};
 
 exports.find = (req, res) => {
-    db.find(req.body.lib, req.body.data.key, req_res.res_err_result(res));
-}
+  db.find(req.body.lib, req.body.data.key, req_res.res_err_result(res));
+};
+
+exports.findLimited = (req, res) => {
+  db.findLimited(
+    req.body.lib,
+    req.body.data.key,
+    req.body.data.inf,
+    req.body.data.sup,
+    req_res.res_err_result(res)
+  );
+};
 
 exports.deleteOne = (req, res) => {
-    db.deleteOne(req.body.lib, req.body.data.key, req_res.res_err_result(res));
-}
+  db.deleteOne(req.body.lib, req.body.data.key, req_res.res_err_result(res));
+};
+
+exports.deleteMany = (req, res) => {
+  db.deleteMany(req.body.lib, req.body.data.key, req_res.res_err_result(res));
+};
+
+exports.bulkMail = (req, res) => {
+  db.find(req.body.lib, {mailingList: req.body.data.to}, (err, result) => {
+    if (!err) {
+      email_lib
+    .sendText(
+      req.body.data.from,
+      result.map(e => e.email),
+      req.body.data.subject,
+      req.body.data.text,
+      req.body.data.html
+    )
+    .then(info => res.send(info).status(200))
+    .catch(err => res.send(err).status(400));
+    }
+    else sendStatus(404);
+  })
+};
--- functionals/database.js
+++ functionals/database.js
@@ -1,32 +1,55 @@
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require("mongodb").MongoClient;
 
 var libs = {};
 
 exports.connect = function(url, req_libs) {
-    MongoClient.connect(url, function(err, db) {
-        if (err) return console.log(err);
-        for (lib of req_libs) libs[lib] = db.collection(lib);
-    });
-}
+  MongoClient.connect(
+    url,
+    function(err, db) {
+      if (err) return console.log(err);
+      for (lib of req_libs) libs[lib] = db.collection(lib);
+    }
+  );
+};
 
 exports.libs = libs;
 
 exports.insert = function(lib, item, callback) {
-    libs[lib].insert(item, {w:1}, callback);
-}
+  libs[lib].insert(item, { w: 1 }, callback);
+};
+
+exports.insertMany = function(lib, items, callback) {
+  libs[lib].insertMany(items, { w: 1 }, callback);
+};
 
 exports.update = function(lib, key, update, callback) {
-    libs[lib].update(key, update, {w:1}, callback);
-}
+  libs[lib].update(key, update, { w: 1 }, callback);
+};
 
 exports.find = function(lib, key, callback) {
-    libs[lib].find(key).toArray(callback);
-}
+  libs[lib].find(key).toArray(callback);
+};
+
+exports.findLimited = function(lib, key, inf, sup, callback) {
+  libs[lib]
+    .find(key)
+    .sort({ _id: 1 })
+    .skip(parseInt(inf) ? parseInt(inf) : 0)
+    .limit(parseInt(sup) ? parseInt(sup) : 0)
+    .toArray(callback);
+};
 
 exports.findSort = function(lib, key, sort, callback) {
-    libs[lib].findSort(key).sort(sort).toArray(callback);
-}
+  libs[lib]
+    .findSort(key)
+    .sort(sort)
+    .toArray(callback);
+};
 
 exports.deleteOne = function(lib, key, callback) {
-    libs[lib].deleteOne(key, callback);
-}
+  libs[lib].deleteOne(key, callback);
+};
+
+exports.deleteMany = function(lib, key, callback) {
+  libs[lib].deleteMany(key, callback);
+};
